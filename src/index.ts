a555b6c1965d7c88fb6d554069a7b7a0a7ddd8a9
Yasmin <yasmin.mota311@gmail.com>
2023-05-08 11:29:41 -0300

    Listagem de alunos com uso de array, apresentando no console nome, idade e nota.

--- src/index.ts
+++ src/index.ts
@@ -39,5 +39,58 @@ app.listen(PORT, () => {
 
 	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
 	// R01, R02, R03, R04, R05
-	
+
+	const readline = require ('readline');
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+
+
+interface Aluno {
+  nome: string;
+  idade: number;
+  nota: number;
+}
+
+const alunos: Aluno[] = [];
+
+const cadastrarAlunos = (quantidade: number, contador: number = 0): void => {
+  if (contador < quantidade) {
+    rl.question(`Digite o nome do aluno ${contador + 1}: `, (nome: string) => {
+      rl.question(`Digite a idade do aluno ${contador + 1}: `, (idade: string) => {
+      rl.question(`Digite a nota do aluno ${contador + 1}: `, (nota: string) => {
+        const notaNumber = Number(nota);
+        const idadeNumber = Number(idade);
+        if (isNaN(notaNumber + idadeNumber)) {
+          console.log('Número inválido, tente novamente!');
+          cadastrarAlunos(quantidade, contador);
+        } else {
+          alunos.push({ nome, idade:idadeNumber, nota: notaNumber });
+          cadastrarAlunos(quantidade, contador + 1);  
+        }
+			});
+		});
+		});
+  } else {
+    console.log('Alunos cadastrados:');
+    alunos.forEach((aluno) => {
+      console.log(`- Nome:${aluno.nome}\n Idade: ${aluno.idade}\n Nota: ${aluno.nota}`);
+    });
+   
+    rl.close();
+  }
+};
+
+rl.question('Quantos alunos deseja inserir? ', (quantidade: string) => {
+  const quantidadeNumber = Number(quantidade);
+  if (isNaN(quantidadeNumber)) {
+    console.log('Quantidade inválida, tente novamente!');
+    rl.close();
+  } else {
+    console.log(`A quantidade digitada foi: ${quantidadeNumber}`);
+    cadastrarAlunos(quantidadeNumber);
+  }
+}); 
+
 });
